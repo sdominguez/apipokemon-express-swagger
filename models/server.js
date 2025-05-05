c5a2cfa019b49915804aabd7e6f118b98f76b7f8
Saul Dominguez <dguezs@gmail.com>
2025-05-05 13:40:37 -0600

    se elimina linea no utilizada

--- models/server.js
+++ models/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const swaggerJsdoc = require('swagger-jsdoc'); 
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../swagger');
 
